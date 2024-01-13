d533933ee910b5d5a35472671b3c1223da92028f
Tucnuc <cer42372@spseol.cz>
2024-01-13 09:56:48 +0100

    Release 2.3

--- Kostky-Hra.py
+++ Kostky-Hra.py
@@ -2,7 +2,7 @@ import time
 import random
 
 # Listy
-loc = ["gamemode"]  # Značí v jaké části se uživatel nachází
+loc = []  # Značí v jaké části se uživatel nachází
 hozenaCisla = []    # Ukládá hozená čísla
 postupka  = [1,2,3,4,5,6]   # Pro porovnávání hodu, kdyby nastala postupka
 pi = set()  # Ukládá čísla z hozeneCisla, pro další porovnání, v případě že by existovala dvojice
@@ -52,27 +52,27 @@ def secondaryPocet(fi, ro, points2):    # Úplně to samé jak předchozí funkc
         return False
 
 # Random úvod
-# postText("\033[1mVítejte!\033[0m")
-# time.sleep(0.8)
-# postText("Děkujeme za zakoupení naší kostkové hry.")
-# time.sleep(0.8)
-# postText("Doufáme, že si ji užijete a přejeme vám \033[1mhodně štěstí\033[0m ve vašich budoucích hodech.")
-# time.sleep(1)
-# print("")
-# startInput1 = postInput("Nejdříve vyberte, kolik chcete hráčů \033[1m[1 - ∞]\033[0m: ")
-# time.sleep(0.8)
-# print("")
-# postText(" ̶̰̮̾͆̕ ̶̢͇̣́͌ ̸͉̖̲̻̩̟̙͖͊E̴̛̛͚̼͓̔̍̎̏͗͘r̵̟̼͎͆̊̆͂̽r̷̗̓̏̏̓̄o̴̠̔̒͂̕r̴̛̬̭̯͎̳̗͊̈́̑̈́ ̴̮̭̭̤̌͘͝o̸̢̰͉͎̹͖̞̓̉͑̋̇͜c̵̱͇̳̻̪̿́̌͋c̵̢̛̤̠͔͇͕͕̍̓̓͝͝ự̴͕͔̰̪̫̝́͑̀̾͠ṛ̴͚̖͓̭̱̿͌̎̈́ͅe̴̢̮̞̞͔̔͌̀d̵̝̤̃́̒̐͆ ̴̨͎̹̪͉̲̗̹̿́̍̔̊̎̚͠ ̷̨̝̬̩̤͋͐̍̽ ̸̧͎͇̳̍̈͜ͅ",0.015)
-# time.sleep(1)
-# print("")
-# postText2("Nyní začneme hru pro")
-# time.sleep(0.5)
-# postText2(" . . . ")
-# time.sleep(0.5)
-# postText("1 hráče.")
-# time.sleep(1)
-# print("")
-# loc.append("gamemode")
+postText("\033[1mVítejte!\033[0m")
+time.sleep(0.8)
+postText("Děkujeme za zakoupení naší kostkové hry.")
+time.sleep(0.8)
+postText("Doufáme, že si ji užijete a přejeme vám \033[1mhodně štěstí\033[0m ve vašich budoucích hodech.")
+time.sleep(1)
+print("")
+startInput1 = postInput("Nejdříve vyberte, kolik chcete hráčů \033[1m[1 - ∞]\033[0m: ")
+time.sleep(0.8)
+print("")
+postText(" ̶̰̮̾͆̕ ̶̢͇̣́͌ ̸͉̖̲̻̩̟̙͖͊E̴̛̛͚̼͓̔̍̎̏͗͘r̵̟̼͎͆̊̆͂̽r̷̗̓̏̏̓̄o̴̠̔̒͂̕r̴̛̬̭̯͎̳̗͊̈́̑̈́ ̴̮̭̭̤̌͘͝o̸̢̰͉͎̹͖̞̓̉͑̋̇͜c̵̱͇̳̻̪̿́̌͋c̵̢̛̤̠͔͇͕͕̍̓̓͝͝ự̴͕͔̰̪̫̝́͑̀̾͠ṛ̴͚̖͓̭̱̿͌̎̈́ͅe̴̢̮̞̞͔̔͌̀d̵̝̤̃́̒̐͆ ̴̨͎̹̪͉̲̗̹̿́̍̔̊̎̚͠ ̷̨̝̬̩̤͋͐̍̽ ̸̧͎͇̳̍̈͜ͅ",0.015)
+time.sleep(1)
+print("")
+postText2("Nyní začneme hru pro")
+time.sleep(0.5)
+postText2(" . . . ")
+time.sleep(0.5)
+postText("1 hráče.")
+time.sleep(1)
+print("")
+loc.append("gamemode")
 
 # Vybírání hracího módu
 if "gamemode" in loc:
@@ -222,7 +222,7 @@ while True:
             continue
         break
 
-# Toto se musí ještě vyžehlit jak stará košile ty vole
+# Dramatický text pro finální počet bodů
 postText2("Sčítáme")
 time.sleep(0.25)
 postText("...",0.25)
@@ -230,16 +230,77 @@ time.sleep(0.8)
 postText("Sčítání bylo \033[1múspěšně\033[0m dokončeno.")
 time.sleep(0.8)
 print("")
-postText2("Získali jste celkem")
+postText2("Získal jste celkem")
 time.sleep(0.5)
 postText2(" . . . ")
 time.sleep(0.8)
 postText(f"\033[1m{mainPoints} bodů\033[0m.")
-time.sleep(1)
-postText2(f"Špatně jste hodil \033[1m{str(len(hozenaCisla))} kostek\033[0m. ")
 time.sleep(0.8)
+
+# Motivační zprávy pro body
+if mainPoints == 0:
+    postText2("Zvládnul jste hodit úplné minimum, co je možné. ")
+    time.sleep(0.8)
+elif mainPoints > 0 and mainPoints <= 100:
+    postText2("Máte se ještě co učit. ")
+    time.sleep(0.8)
+elif mainPoints > 100 and mainPoints < 2000:
+    postText2("Jde vidět, že už jste někdy hrál kostky. ")
+    time.sleep(0.8)
+elif mainPoints > 2000 and mainPoints < 8000:
+    postText2("Opravdu nádherný výkon! ")
+    time.sleep(0.8)
+else:
+    postText2("Podařilo se vám hodit maximum bodů! ")
+    time.sleep(0.8)
+postText("\033[1mGratulujeme!\033[0m")
+time.sleep(1)
+print("")
+
+# Motivační zprávy pro kostky
 nevimUz = ', '.join(str(cislo3) for cislo3 in hozenaCisla[:])
-postText(f"Zbyli na nich čísla: \033[1m{nevimUz}\033[0m")
+if len(hozenaCisla) == 6:
+    postText2(f"Špatně jste hodil všech \033[1m{str(len(hozenaCisla))} kostek\033[0m. ")
+    time.sleep(0.8)
+    postText(f"Zbyli na nich čísla: \033[1m{nevimUz}\033[0m.")
+    time.sleep(0.8)
+    postText2("Každý je dobrý v něčem svém! ")
+    time.sleep(0.8)
+    postText("\033[1mNezoufejte!\033[0m")
+elif len(hozenaCisla) == 5:
+    postText2(f"Špatně jste hodil \033[1m{str(len(hozenaCisla))} kostek\033[0m. ")
+    time.sleep(0.8)
+    postText(f"Zbyli na nich čísla: \033[1m{nevimUz}\033[0m.")
+    time.sleep(0.8)
+    postText2("To že se vám nedaří, může být někdy samo o sobě také štěstí! ")
+    time.sleep(0.8)
+    postText("\033[1mHodně štěstí příště!\033[0m")
+elif len(hozenaCisla) < 5 and len(hozenaCisla) > 1:
+    postText2(f"Špatně jste hodil \033[1m{str(len(hozenaCisla))} kostky\033[0m. ")
+    time.sleep(0.8)
+    postText(f"Zbyli na nich čísla: \033[1m{nevimUz}\033[0m.")
+    time.sleep(0.8)
+    postText2("Už se do toho dostáváte! ")
+    time.sleep(0.8)
+    postText("\033[1mPříště to bude lepší!\033[0m")
+elif len(hozenaCisla) == 1:
+    postText2(f"Špatně jste hodil pouze \033[1m{str(len(hozenaCisla))} kostku\033[0m. ")
+    time.sleep(0.8)
+    postText(f"Zbylo na ni číslo: \033[1m{nevimUz}\033[0m.")
+    time.sleep(0.8)
+    postText2("Opravdu krásná práce! ")
+    time.sleep(0.8)
+    postText("\033[1mJen tak dál!\033[0m")
+else:
+    postText2(f"Špatně jste hodil neuvěřitelných \033[1m{str(len(hozenaCisla))} kostek\033[0m. ")
+    time.sleep(0.8)
+    postText(f"Žádné číslo nezbylo.")
+    time.sleep(0.8)
+    postText2("Jste opravdu certifikovaný házeč kostek! ")
+    time.sleep(0.8)
+    postText("\033[1mJen tak dál!\033[0m")
+
+
 
 
 
